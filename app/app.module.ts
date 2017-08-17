379538cf0ddb838cbb3756837ee16b22ff8c13e7
zeus <angelctmex@gmail.com>
2017-08-17 10:08:47 -0500

    Commit inicial, para aprender angular 2

---
+++ app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import ApplicationComponent from './components/application/application';
+import StarsComponent from './components/stars/stars';
+import CarouselComponent from './components/carousel/carousel';
+import FooterComponent from  './components/footer/footer';
+import NavbarComponent from './components/navbar/navbar';
+import {ProductService} from "./services/product-service";
+
+
+@NgModule({
+    imports:      [ BrowserModule ],
+    declarations: [ ApplicationComponent,
+                    StarsComponent,
+                    NavbarComponent,
+                    CarouselComponent,
+                    FooterComponent
+                  ],
+    providers:    [ProductService],
+    bootstrap:    [ ApplicationComponent ]
+})
+export class AppModule { }
