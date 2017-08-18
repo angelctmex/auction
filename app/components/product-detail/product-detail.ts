8cccbbba63d9a46178832b932932888c893a2108
zeus <angelctmex@gmail.com>
2017-08-18 13:35:33 -0500

    Implementando la visualizacion del detalle de productos

--- app/components/product-detail/product-detail.ts
+++ app/components/product-detail/product-detail.ts
@@ -1,20 +1,20 @@
-import {Component} from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
+import {Component} from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import {Product, Review, ProductService} from '../../services/product-service';
 
 @Component({
     selector: 'auction-product-page',
-    template: `
-        <div>
-            <img src="http://placehold.it/820x320">
-            <h4>{{productTitle}}</h4>
-        </div>
-    `
+    templateUrl: 'app/components/product-detail/product-detail.html'
 })
+export default class ProductDetailComponent {
+    product: Product;
+    reviews: Review[];
 
-export default class ProductDetailComponent{
-    productTitle: string;
+    constructor(route: ActivatedRoute, productService: ProductService) {
 
-    constructor( route: ActivatedRoute ){
-        this.productTitle = route.snapshot.params['prodTitle'];
+        let prodId: number = parseInt(route.snapshot.params['productId']);
+        this.product = productService.getProductById(prodId);
+
+        this.reviews = productService.getReviewsForProduct(this.product.id);
     }
 }
